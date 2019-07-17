28a2a2657c620a1d26d8f64033faf38852e57021
matthewhopwood <matthewhopwood21@hotmail.com>
2019-07-17 11:21:30 +0100

    updated registrationscontroller

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,10 +1,14 @@
 class User::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters
+  before_action :create_configure_permitted_parameters, only: [:create]
+  before_action :update_configure_permitted_parameters, only: [:update]
 
   protected
 
-  def configure_permitted_parameters
+  def create_configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name])
+  end
+  
+  def update_configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys:[:first_name, :last_name])
   end
 end
