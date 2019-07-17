3cdbb75f84c6a7e23f4210e78de356cea81eeb00
matthewhopwood <matthewhopwood21@hotmail.com>
2019-07-17 09:29:40 +0100

    Created user_stocks table, controller and model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,7 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
- 
- 
+
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
@@ -18,4 +17,5 @@ class Stock < ApplicationRecord
       return nil
     end
   end
+  
 end
