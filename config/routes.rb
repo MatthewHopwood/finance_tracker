e3c9b31a9ed472f4c9a11dd2e292e00d5bed2ab5
matthewhopwood <matthewhopwood21@hotmail.com>
2019-07-18 11:19:37 +0100

    Add friend and remove friend functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'my_friends', to: "users#my_friends"
   get 'search_stocks', to: "stocks#search"
   get 'search_friends', to: "users#search"
+  post 'add_friend', to: "users#add_friend"
 
 
 end
